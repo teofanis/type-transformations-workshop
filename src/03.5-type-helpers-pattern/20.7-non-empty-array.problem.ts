60e46cf246c906d00c62f6dba4827e9e10d77a36
Teofanis Papadopulos <teos_97@hotmail.com>
2024-06-10 18:26:32 +0100

    Completed 20.7

--- src/03.5-type-helpers-pattern/20.7-non-empty-array.problem.ts
+++ src/03.5-type-helpers-pattern/20.7-non-empty-array.problem.ts
@@ -1,4 +1,4 @@
-type NonEmptyArray = unknown;
+type NonEmptyArray<T> = [T, ...T[]] // At least 1, at least 2 (T, T, ...T[]) etc... 
 
 export const makeEnum = (values: NonEmptyArray<string>) => {};
 
