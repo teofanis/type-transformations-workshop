877baa5175b8c746785187ad1d93f491b4bac003
Teofanis Papadopulos <teos_97@hotmail.com>
2024-06-10 21:24:18 +0100

    Completed 24

--- src/04-conditional-types-and-infer/24-infer-with-generics.problem.ts
+++ src/04-conditional-types-and-infer/24-infer-with-generics.problem.ts
@@ -14,6 +14,6 @@ type Example = MyComplexInterface<
   { x: 12; y: 14 }
 >;
 
-type GetPoint<T> = unknown;
-
+type GetPoint<T> = T extends MyComplexInterface<infer TEvent, infer TContext, infer TName,infer TPoint> ? TPoint : never
+ 
 type tests = [Expect<Equal<GetPoint<Example>, { x: 12; y: 14 }>>];
