db16eb022145b32cc5b174413173db887da210db
Teofanis Papadopulos <teos_97@hotmail.com>
2024-06-11 16:02:14 +0100

    complete 37

--- src/06-challenges/37-get-dynamic-path-params.problem.ts
+++ src/06-challenges/37-get-dynamic-path-params.problem.ts
@@ -1,10 +1,15 @@
+import { S } from "ts-toolbelt";
 import { Equal, Expect } from "../helpers/type-utils";
 
 type UserPath = "/users/:id";
 
 type UserOrganisationPath = "/users/:id/organisations/:organisationId";
 
-type ExtractPathParams = unknown;
+ type ExtractPathParams<T extends string > = {
+  [Part in S.Split<T, '/'>[number] as Part extends `:${infer Segment}` ? Segment : never ]: string;
+ }
+ 
+type Example = ExtractPathParams<UserPath>
 
 type tests = [
   Expect<Equal<ExtractPathParams<UserPath>, { id: string }>>,
